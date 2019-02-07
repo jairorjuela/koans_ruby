5c26981144e6f9c9b4b87d19ffe55e4f44243415
jairorjuela <genius85862@gmail.com>
2019-02-06 21:48:22 -0500

    triangle

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,16 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == 0 && b == 0 && c == 0
+    "Error"
+  elsif a == b && b == c
+    :equilateral
+  elsif (a == b && b!= c) || (b == c && a!= c)
+    :isosceles
+  else a =! b && b != c && c!= a
+    :scalene
+  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
