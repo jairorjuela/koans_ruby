86ddd6b3fb09f8fe35788c2a419e0008f1f694c9
jairorjuela <genius85862@gmail.com>
2019-02-19 22:00:33 -0500

    About Classes

--- about_classes.rb
+++ about_classes.rb
@@ -130,7 +130,7 @@ class AboutClasses < Neo::Koan
     fido = Dog6.new("Fido")
     rover = Dog6.new("Rover")
 
-    assert_equal __, rover.name != fido.name
+    assert_equal true, rover.name != fido.name
   end
 
   # ------------------------------------------------------------------
@@ -159,7 +159,7 @@ class AboutClasses < Neo::Koan
     fido = Dog7.new("Fido")
 
     fidos_self = fido.get_self
-    assert_equal __, fidos_self
+    assert_equal fido, fidos_self
   end
 
   def test_to_s_provides_a_string_version_of_the_object
