c09f12e56517c54744751f2d3849a0408abfb874
jairorjuela <genius85862@gmail.com>
2019-02-24 20:51:51 -0500

    Vulnerability

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -3,10 +3,11 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Implement a DiceSet Class here:
 #
 class DiceSet
+  attr_reader :values
   def roll(value)
-    dice = {}
-    dice.merge!(key: value)
+    @values = Array.new
   end
+  @values
 end
 
 class AboutDiceProject < Neo::Koan
